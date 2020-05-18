ca545bbf7b67d898bba41560a43f3a6294624500
Jean Paul <jeanpaul2022@gmail.com>
2020-05-18 14:48:15 -0300

    Criando ambiente e adicionando primeiro teste de animação

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { View, Image, Dimensions, Animated, TouchableNativeFeedback } from 'react-native';
 
 export default function App() {
+  const [width, setWidth] = useState(Dimensions.get('window').width)
+  const [height, setHeight] = useState(Dimensions.get('window').height)
+  const fadeAnimWidth = useRef(new Animated.Value(width)).current;
+  const fadeAnimHeight = useRef(new Animated.Value(height)).current;
+
+  const fadeIn = () => {
+    // Will change fadeAnim value to 1 in 5 seconds
+    Animated.sequence([
+      Animated.timing(fadeAnimWidth, {
+        toValue: 200,
+        duration: 1000
+      }),
+      Animated.timing(fadeAnimHeight, {
+        toValue: 200,
+        duration: 1000
+      })
+    ]).start();
+  };
+
+  const fadeOut = () => {
+    // Will change fadeAnim value to 0 in 5 seconds
+    Animated.timing(fadeAnimWidth, {
+      toValue: 200,
+      duration: 400
+    }).start();
+    Animated.timing(fadeAnimHeight, {
+      toValue: 200,
+      duration: 400
+    }).start();
+  };
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Jean Paul...</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', }}>
+      <TouchableNativeFeedback onPressIn={fadeIn} onPressOut={fadeOut}>
+        <Animated.Image
+          style={{ height: fadeAnimHeight, width: fadeAnimWidth }}
+          resizeMode={'contain'}
+          source={{
+            uri: 'https://images.unsplash.com/photo-1589729306025-c433a3c53a64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'
+          }} />
+      </TouchableNativeFeedback>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
