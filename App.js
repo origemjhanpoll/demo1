fc834e6babc5c660cbedc2f94c96aa0d7d7ffcb7
Jean Paul <jeanpaul2022@gmail.com>
2020-06-01 17:04:06 -0300

    add reanimated

--- App.js
+++ App.js
@@ -1,50 +1,35 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { View, Image, Dimensions, Animated, TouchableOpacity, FlatList, TouchableNativeFeedback } from 'react-native';
+import { View, Image, Dimensions, TouchableOpacity, FlatList, TouchableNativeFeedback, Text, StyleSheet, } from 'react-native';
+
 
 export default function App() {
   const [width, setWidth] = useState(Dimensions.get('window').width)
   const [height, setHeight] = useState(Dimensions.get('window').height)
-  const fadeAnimWidth = useRef(new Animated.Value(width)).current;
-  const fadeAnimHeight = useRef(new Animated.Value(height)).current;
   const [animation, setAnimation] = useState(false)
-  const [saveIndex, setSaveIndex] = useState(null)
-
-  const data = [
-    { uri: 'https://images.unsplash.com/photo-1589729306025-c433a3c53a64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80' },
-    { uri: 'https://images.unsplash.com/photo-1589780416490-904aa1b3d242?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80' },
-  ]
-  useEffect(() => {
-    if (animation) {
-      Animated.timing(fadeAnimHeight, {
-        toValue: height,
-        duration: 400
-      }).start();
-    } else {
-      Animated.timing(fadeAnimHeight, {
-        toValue: height / 3,
-        duration: 400
-      }).start();
-    }
-  }), [animation];
-
+  const uri = useRef('https://images.unsplash.com/photo-1590962677400-f97466952836?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80').current
 
   return (
-    <FlatList
-      data={data}
-      keyExtractor={(item, index) => 'key' + index}
-      renderItem={({ item, index }) =>
-        <TouchableNativeFeedback onPress={() => [setAnimation(!animation), setSaveIndex(index)]}>
-          <Animated.Image
-            style={saveIndex == index ?
-              { height: fadeAnimHeight, width: width, }
-              :
-              { height: height / 3, width: width, }
-            }
-            source={{
-              uri: item.uri
-            }} />
-        </TouchableNativeFeedback>
-      }
-    />
+    <View style={styles.container}>
+      <Image source={{ uri: uri }} style={{ width: width, height: 300 }} />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: 'black',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    margin: 8
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+});
+
