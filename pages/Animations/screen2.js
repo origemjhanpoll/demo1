4ce2c3cd192b667a0a778f3e1d3cf271394baa6d
Jean Paul | Origem <jeanpaul2022@gmail.com>
2020-08-05 00:22:50 -0300

    Testando FlatList e TextInput

--- pages/Animations/screen2.js
+++ pages/Animations/screen2.js
@@ -1,19 +1,127 @@
 import React, { useEffect, useState, useRef } from "react";
-import { View, Image, TouchableOpacity, StyleSheet, Text } from "react-native";
+import { View, Image, KeyboardAvoidingView, StyleSheet, Text, FlatList, TextInput } from "react-native";
 import { Actions } from "react-native-router-flux";
 
 export default function Screen2() {
+  const data = [
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+    { nome: 'jean' },
+    { nome: 'paul' },
+    { nome: 'vale' },
+  ]
   return (
-    <TouchableOpacity style={styles.container} onPress={() => Actions.home()}>
-      <Text>animation2</Text>
-    </TouchableOpacity >
+    <KeyboardAvoidingView behavior='padding' style={{ flex: 1 }}>
+      <FlatList
+        keyExtractor={(item, index) => String(item.nome + index)}
+        data={data}
+        renderItem={({ item }) => <Text>{item.nome}</Text>}
+        style={{ backgroundColor: 'red' }}
+      />
+      <View style={{ height: 80, padding: 16 }}>
+        <TextInput style={{ height: 48, borderWidth: 2, padding: 10 }} />
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
